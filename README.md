b731636cf4cd2d490071446074effc47f900fac0
Adan Gutierrez <hevyw8@gmail.com>
2013-10-19 17:35:00 -0500

    First commit and README update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bloccit
+
+A Reddit-Style application to demonstrate foundation programming and web development.
+
+Made by Adan Gutierrez
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdanBloccit::Application.config.secret_token = 'ccd029a7ccfd39bafa7c60eb492caea36a9704ded96868c1a7f125b6192f0dbd84623da2c7225eeff45d4835819cf0bdc410bad4923f65ee707c443bcc6b1d69'
